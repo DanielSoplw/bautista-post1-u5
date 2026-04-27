d8c47ce8846f9a26df0d39259592e5ec220d7e7c
DaniSoplw <danieleduardobautistap@gmail.com>
2026-04-26 23:48:04 -0500

    feat: configuracion inicial con SpringDoc y Swagger UI

--- src/main/java/com/universidad/patrones/BibliotecaApiApplication.java
+++ src/main/java/com/universidad/patrones/BibliotecaApiApplication.java
@@ -3,7 +3,7 @@ package com.universidad.patrones;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication (scanBasePackages = "com.universidad.patrones")
 public class BibliotecaApiApplication {
 
     public static void main(String[] args) {
